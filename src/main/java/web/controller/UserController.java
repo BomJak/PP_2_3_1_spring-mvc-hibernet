1ef9122af33f634030001c1cd2fed109f19ff4fe
BomJak <denis250597@yandex.ru>
2025-01-14 03:13:18 +0300

    up Controller

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,7 +3,6 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,7 @@ import javax.validation.Valid;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
