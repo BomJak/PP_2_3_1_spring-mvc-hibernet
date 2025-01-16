8fc0834ff818e4669123c919fbdecc59faa3923c
BomJak <denis250597@yandex.ru>
2025-01-16 12:15:09 +0300

    update

--- src/main/java/web/DAO/UserDAOImp.java
+++ src/main/java/web/DAO/UserDAOImp.java
@@ -1,7 +1,6 @@
 package web.DAO;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import web.models.User;
 
 import javax.persistence.EntityManager;
@@ -11,22 +10,19 @@ import java.util.List;
 @Repository
 public class UserDAOImp implements UserDAO {
     @PersistenceContext
-    public EntityManager entityManager;
+    private EntityManager entityManager;
 
     @Override
-    @Transactional
     public void addUser(User user) {
         entityManager.persist(user);
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         entityManager.merge(user);
     }
 
     @Override
-    @Transactional
     public void deleteUser(int id) {
         User user = findById(id);
         if(user != null) {
@@ -35,13 +31,11 @@ public class UserDAOImp implements UserDAO {
     }
 
     @Override
-    @Transactional
     public User findById(int id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<User> listUsers() {
         return entityManager.createQuery("from User", User.class).getResultList();
     }
--- src/main/java/web/services/CarService.java
+++
@@ -1,9 +0,0 @@
-package web.services;
-import web.models.Cars;
-
-import java.util.List;
-
-public interface CarService {
-    public List<Cars> getByNumber(int num);
-    public List<Cars> getAllCars();
-}
